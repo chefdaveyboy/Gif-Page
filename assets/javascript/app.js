3f6fed0823aa334ca98f59f78aa3f51ccbb089f7
David Koeller <david.koeller@gmail.com>
2020-01-17 21:13:38 -0600

    styled, js works, pause doesn't work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,26 +27,115 @@ $("#search-button").on("click", function(event) {
 
     var searchValue = $("#search-form").val().trim();
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchValue + "&api_key=p18dA5Q1Xvc80bSdVEBrgSxQu6v3Rlmy&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchValue + "&api_key=p18dA5Q1Xvc80bSdVEBrgSxQu6v3Rlmy&limit=15";
 
     teamArray.push(searchValue);
+
+    $("#gif-display").empty();
    
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function(response) {
-
         console.log(response);
+
+        
+    
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        gifDiv.addClass("gif-div");
+        var ratingP = $("<p>").text("Rating: " + results[i].rating);
+            if (results[i].rating === "g") {
+                ratingP.css("background-color", "green");
+                ratingP.css("color", "white");
+            }
+            else if (results[i].rating === "pg") {
+                ratingP.css("background-color", "yellow");
+                ratingP.css("color", "black");
+            }
+            else if (results[i].rating === "pg-13") {
+                ratingP.css("background-color", "orange");
+                ratingP.css("color", "black");
+            }
+            else if (results[i].rating === "r") {
+                ratingP.css("background-color", "red");
+                ratingP.css("color", "white");
+            }
+        var gifImage = $("<img>");
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifDiv.append(ratingP);
+        gifDiv.append(gifImage);
+        $("#gif-display").prepend(gifDiv);
+    }
+
 
 
     });
-
     createButtons();
 
 });
 
-// $(document).on("click", ".team", event);
+$("#favorites").on("click", ".team", function(event) {
+    event.preventDefault();
+
+    var buttonValue = $(this).data("team");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonValue + "&api_key=p18dA5Q1Xvc80bSdVEBrgSxQu6v3Rlmy&limit=15";
+
+    $("#gif-display").empty();
+
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        console.log(response);
+
+        var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        gifDiv.addClass("gif-div");
+        var ratingP = $("<p>").text("Rating: " + results[i].rating);
+        if (results[i].rating === "g") {
+            ratingP.css("background-color", "green");
+            ratingP.css("color", "white");
+        }
+        else if (results[i].rating === "pg") {
+            ratingP.css("background-color", "yellow");
+            ratingP.css("color", "black");
+        }
+        else if (results[i].rating === "pg-13") {
+            ratingP.css("background-color", "orange");
+            ratingP.css("color", "black");
+        }
+        else if (results[i].rating === "r") {
+            ratingP.css("background-color", "red");
+            ratingP.css("color", "white");
+        }
+        var gifImage = $("<img>");
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifDiv.append(ratingP);
+        gifDiv.append(gifImage);
+        $("#gif-display").prepend(gifDiv);
+        
+        $("#gif-display").on("click", function() {
+            $(src).attr(results[i].images.fixed_height.url);
+
+        })
+
+    }
+    });
+
+
+    console.log(buttonValue);
+});
+
+
 
 
 createButtons();
